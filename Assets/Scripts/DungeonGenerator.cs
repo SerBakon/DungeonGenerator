fe89a68e075307e84935c5fd4171f1fdbc855649
SerBakon <serbakonyt@gmail.com>
2025-06-22 20:55:38 -0700

    Feat: starter room

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -12,25 +12,42 @@ public class DungeonGenerator : MonoBehaviour
     [SerializeField] private int DungeonSize = 25;
 
     [SerializeField] private GameObject floorTile;
-    
+    [SerializeField] private GameObject starterTile;
+
     private HashSet<Vector3Int> visited = new HashSet<Vector3Int>();
     void Start()
     {
-        floorGen();
+        generate();
     }
 
-    private void floorGen() {
+    private void generate() {
         roomGen(numRooms);
-        foreach (var floor in visited) {
-            Instantiate(floorTile, new Vector3(floor.x,floor.y,floor.z), Quaternion.identity);
+        starterGen(starterTile);
+    }
+    private void floorGen(GameObject tile, HashSet<Vector3Int> floorPos) {
+        foreach (var floor in floorPos) {
+            Instantiate(tile, new Vector3(floor.x,floor.y,floor.z), Quaternion.identity);
         }
     }
 
     private void roomGen(int numRooms) {
+        HashSet<Vector3Int> floorPos = new HashSet<Vector3Int>();
         for (int i = 0; i < numRooms; i++) {
             Vector3Int roomCenter = new Vector3Int(Random.Range(-1 * DungeonSize, DungeonSize), 0, Random.Range(-1 * DungeonSize, DungeonSize));
-            HashSet<Vector3Int> floorPos = RandomWalk.randomWalk(roomCenter, walkLength, visited);
-            visited.UnionWith(floorPos);
+            floorPos.UnionWith(RandomWalk.randomWalk(roomCenter, walkLength, visited));
         }
+        visited.UnionWith(floorPos);
+        floorGen(floorTile, floorPos);
+    }
+
+    private void starterGen(GameObject tile) {
+        HashSet<Vector3Int> floorPos = new HashSet<Vector3Int>();
+        for (int i = -2; i <= 2; i++) {
+            for (int j = -2; j <= 2; j++) {
+                floorPos.Add(new Vector3Int(i, 0, j));
+            }
+        }
+        visited.UnionWith(floorPos);
+        floorGen(tile, floorPos);
     }
 }
